26988bbd9fd5066e819d33cca6d34b120e46498a
Robi <sikkelh@gmail.com>
2021-10-19 13:40:56 +0300

    add app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+const currentImage = document.querySelector("#image");
+
+const images = ["images/pilt1.jpg", "images/pilt2.jpg", "images/pilt3.jpg"];
+
+function changeImage() {
+    //console.log("Hello from script file!"); LATER DELETE kui "const images =" tehtud
+
+    let randomNumber = Math.floor(Math.random() * images.length);
+    console.log(randomNumber);
+    currentImage.src = images[randomNumber];
+}
